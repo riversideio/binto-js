99094ca0623fe65824c98040c83f9d746706349d
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-01-11 14:09:35 -0800

    decoupling handlebars and requirejs fixes #1 fixes #2

--- sdk.js
+++ sdk.js
@@ -1,113 +1,139 @@
-define('io', ['jquery', 'handlebars'], function ( $, Handlebars ) {
-	var user,
-		processing;
+(function ( exports ) {
 
-	function _sendRequest ( endpoint, data, callback, options ) {
+    function ioSdk( $ ) {
 
-		if ( user ) {
-			data = data || {};
-			data.session_token = user.session_token;
-		}
+        var user,
+            processing,
+            delimiterStart = '{{',
+            delimiterEnd = '}}',
+            pattern = new RegExp( delimiterStart + '(.+?)' + delimiterEnd, 'g');
 
-		return $.ajax({
-			// abstract url to allow for better dev
-			url : 'https://victoria-club.herokuapp.com/api/v0/' + endpoint,
-			data : data,
-			type : options.method || "post",
-			success : function ( res ) {
-				if ( callback ) {
-					if ( res.success ) {
-						callback( null, res );
-						if ( 
-							/\/sessions.json$/g.test( this.url ) ||
-							( 
-								/\/users.json$/g.test( this.url ) && 
-								this.type === "POST" 
-							)
-						){
-							user = res.user;
-						}
-					}else{
-						callback( res );
-					}
-				}
-			},
-			error : function ( err ) {
-				if ( callback ) callback ( err );
-			}
-		})
-	}
+        function templating ( str, vars ) {
+            function getVars ( item ) {
+                // this is kinda cray
+                // replace delimiters and grab var
+                return vars[ item.replace(delimiterStart, '')
+                    .replace(delimiterEnd, '') ];
+            }
+            var items = str.match( pattern ),
+                values = items.map( getVars );
 
-	function _setCall ( endpoint, options ) {
-		options = options || {};
-		if ( options.endpointVars ) {
-			options.compiled = Handlebars.compile( endpoint );
-		}
-		return function ( values, callback ) {
+            for( var i = 0; i < items.length; i += 1 ) {
+                str = str.replace( items[i], values[i] );
+            }
+            return str;
+        }
 
-			var _endpoint;
+        function _sendRequest ( endpoint, data, callback, options ) {
 
-			if ( user ) {
-				values.id = values.id || user.id;
-			}
+            if ( user ) {
+                data = data || {};
+                data.session_token = user.session_token;
+            }
 
-			if ( typeof values === 'function' ) {
-				callback = values;
-				values = {};
-			}
+            return $.ajax({
+                // abstract url to allow for better dev
+                url : 'https://victoria-club.herokuapp.com/api/v0/' + endpoint,
+                data : data,
+                type : options.method || "post",
+                success : function ( res ) {
+                    if ( callback ) {
+                        if ( res.success ) {
+                            callback( null, res );
+                            if ( 
+                                /\/sessions.json$/g.test( this.url ) ||
+                                ( 
+                                    /\/users.json$/g.test( this.url ) && 
+                                    this.type === "POST" 
+                                )
+                            ){
+                                user = res.user;
+                            }
+                        }else{
+                            callback( res );
+                        }
+                    }
+                },
+                error : function ( err ) {
+                    if ( callback ) callback ( err );
+                }
+            })
+        }
 
-			_endpoint = options.endpointVars ? 
-				options.compiled( values ) :
-				endpoint;
+        function _setCall ( endpoint, options ) {
+            options = options || {};
+            return function ( values, callback ) {
 
-			if ( !processing ) {
-				processing = true;
-				return _sendRequest( _endpoint, values, function ( ) {
-					processing = false;
-					if ( callback ) callback.apply( null, arguments );
-				}, options || {} );
-			}
-		}
-	}
+                var _endpoint;
 
-	return {
-		users : {
-			login : _setCall('sessions.json'),
-			create : _setCall('users.json'),
-      all : _setCall('users.json', {
-        method : 'get'
-      }),
-			read : _setCall('users.json', { 
-				method : 'get' 
-			}),
-			update : _setCall('users/{{id}}/update.json', {
-				endpointVars : true
-			}),
-			show : _setCall('users/{{id}}/show.json', {
-				endpointVars : true,
-				method : 'get'
-			}),
-			updateCard : _setCall('users/{{id}}/update_card.json', {
-				endpointVars : true
-			}),
-			checkin : _setCall('checkins.json'),
-			resetPassword : _setCall('users/reset_password.json'),
-			cancelPlan : _setCall('users/{{id}}/cancel_plan.json', {
-				endpointVars : true
-			}),
-			updatePlan : _setCall('users/{{id}}/update_plan.json', {
-				endpointVars : true
-			}),
-			charge : _setCall('users/{{id}}/charge.json', {
-        		endpointVars : true
-      		})
-		},
-		plans : {
-			all : _setCall('plans.json', {
-        method : 'get'
-      })
-		}
+                if ( user ) {
+                    values.id = values.id || user.id;
+                }
 
-	}
+                if ( typeof values === 'function' ) {
+                    callback = values;
+                    values = {};
+                }
 
-});
+                _endpoint = options.endpointVars ? 
+                    templating( endpoint, values ) :
+                    endpoint;
+
+                if ( !processing ) {
+                    processing = true;
+                    return _sendRequest( _endpoint, values, function ( ) {
+                        processing = false;
+                        if ( callback ) callback.apply( null, arguments );
+                    }, options || {} );
+                }
+            }
+        }
+
+        return {
+            users : {
+                login : _setCall('sessions.json'),
+                create : _setCall('users.json'),
+            all : _setCall('users.json', {
+              method : 'get'
+            }),
+                read : _setCall('users.json', { 
+                    method : 'get' 
+                }),
+                update : _setCall('users/{{id}}/update.json', {
+                    endpointVars : true
+                }),
+                show : _setCall('users/{{id}}/show.json', {
+                    endpointVars : true,
+                    method : 'get'
+                }),
+                updateCard : _setCall('users/{{id}}/update_card.json', {
+                    endpointVars : true
+                }),
+                checkin : _setCall('checkins.json'),
+                resetPassword : _setCall('users/reset_password.json'),
+                cancelPlan : _setCall('users/{{id}}/cancel_plan.json', {
+                    endpointVars : true
+                }),
+                updatePlan : _setCall('users/{{id}}/update_plan.json', {
+                    endpointVars : true
+                }),
+                charge : _setCall('users/{{id}}/charge.json', {
+                    endpointVars : true
+                })
+            },
+            plans : {
+                all : _setCall('plans.json', {
+                    method : 'get'
+                })
+            }
+
+        }
+
+    }
+    if ( typeof define === 'function' ) {
+        return define( 'io', ['jquery'], ioSdk )
+    }
+    // globals
+    exports.io = ioSdk( jQuery );
+
+}( this ))
